9028ebb3d458088f516149877c181a109f65c3b8
Metin Online <metonline@gmail.com>
2026-01-17 02:33:49 +0300

    Nuclear option: minimal Flask app

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-#!/usr/bin/env python3
-import os
-import json
-from flask import Flask, send_file
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return send_file('index.html')
-
-@app.route('/api/data')
-def get_data():
-    with open('database.json', 'r', encoding='utf-8') as f:
-        return send_file('database.json', mimetype='application/json')
-
-@app.route('/<path:path>')
-def serve_files(path):
-    try:
-        return send_file(path)
-    except:
-        return send_file('index.html')
+def hello():
+    return "OK", 200
 
 if __name__ == '__main__':
-    import sys
-    host = '0.0.0.0'
+    import os
     port = int(os.environ.get('PORT', 5000))
-    debug = '--debug' in sys.argv
-    app.run(host=host, port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port)
