bf8fdad7011181e9cb88c3067d007596e4c07f04
Metin Online <metonline@gmail.com>
2026-01-17 02:31:23 +0300

    Fix: Clean startup - use PORT env var directly

--- app.py
+++ app.py
@@ -22,5 +22,8 @@ def serve_files(path):
         return send_file('index.html')
 
 if __name__ == '__main__':
+    import sys
+    host = '0.0.0.0'
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
+    debug = '--debug' in sys.argv
+    app.run(host=host, port=port, debug=debug)
